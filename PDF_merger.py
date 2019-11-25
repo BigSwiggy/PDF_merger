2b83ede17065014e24b4cdd10d923f724f3243d7
BigSwiggy <bigswiggy@gmail.com>
2019-11-25 17:09:32 +0100

    :added comment line 4

--- PDF_merger.py
+++ PDF_merger.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfFileMerger
 import os 
 
-path = r"C:/Users/julie/OneDrive/Bureau/PDF_to_merge/"
+path = r"C:/Users/julie/OneDrive/Bureau/PDF_to_merge/" #We need to create somewhere à directory where we put our PDF to merge and give the path of this directory here
 pdf_files = ['1.pdf','2.pdf','3.pdf'] #Here we need the names of the pdf to merge
 merger = PdfFileMerger()
 
